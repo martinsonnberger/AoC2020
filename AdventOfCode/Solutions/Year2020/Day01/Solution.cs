1f5b8d0e606f2b86aedd651248c126e2f48e87fe
martinsonnberger <martin.sonnberger@icloud.com>
2020-12-01 08:34:32 +0100

    day 1 part 2

--- AdventOfCode/Solutions/Year2020/Day01/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day01/Solution.cs
@@ -31,6 +31,20 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartTwo()
         {
+            int[] input = Utilities.ToIntArray(Input, "\n");
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = i; j < input.Length; j++)
+                {
+                    for (int k = j; k < input.Length; k++)
+                    {
+                        if (input[i] + input[j] + input[k] == 2020)
+                        {
+                            return (input[i] * input[j] * input[k]).ToString();
+                        }
+                    }
+                }
+            }
             return null;
         }
     }
