cd1e49425c5662a65b05556bde9c8541ebd66c90
martinsonnberger <martin.sonnberger@icloud.com>
2020-12-04 07:05:57 +0100

    day 4 part 1

--- AdventOfCode/Solutions/Year2020/Day04/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day04/Solution.cs
@@ -15,7 +15,29 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartOne()
         {
-            return null;
+            string[] passports = Input.Split("\n\n");
+            int count = 0;
+            for (int i = 0; i < passports.Length; i++)
+            {
+                if (ValidPassport(passports[i]))
+                {
+                    count++;
+                }
+            }
+            return count.ToString();
+        }
+
+        static bool ValidPassport(string passport)
+        {
+            string[] fields = new string[] {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
+            for (int i = 0; i < fields.Length; i++)
+            {
+                if (passport.Contains(fields[i]) == false)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         protected override string SolvePartTwo()
