14e4a76f565f1819d8a9d17ea3aec556b9ed8ca7
martinsonnberger <martin.sonnberger@icloud.com>
2020-12-05 06:59:12 +0100

    day 5

--- AdventOfCode/Solutions/Year2020/Day05/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day05/Solution.cs
@@ -15,7 +15,55 @@ namespace AdventOfCode.Solutions.Year2020
 
         protected override string SolvePartOne()
         {
-            return null;
+            string[] passes = Input.SplitByNewline();
+            int max = 0;
+            int[,] usedSeats = new int[110, 9]; 
+            for (int i = 0; i < passes.Length; i++)
+            {
+                string rowBinary = passes[i].Substring(0,7).Replace('F', '0').Replace('B', '1');
+                int row = reverseBinary(rowBinary);
+
+                string seatBinary = passes[i].Substring(7).Replace('L', '0').Replace('R', '1');
+                int seat = reverseBinary(seatBinary);
+                usedSeats[row, seat] = 1;
+                int id = row * 8 + seat;
+                if (id > max)
+                {
+                    max = id;
+                }
+            }
+
+            for (int i = 1; i < 110; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (usedSeats[i, j] == 0 && usedSeats[i - 1, j] != 0)
+                    {
+                        Console.WriteLine(i + " " + j);
+                    }
+                }
+            }
+            return max.ToString();
+        }
+
+        static int reverseBinary(string input)
+        {
+            int lo = 1;
+            int hi = (int) Math.Pow(2, input.Length);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int mid = lo + (hi - lo) / 2;
+                if (input[i] == '0')
+                {
+                    hi = mid;
+                }
+                else if (input[i] == '1')
+                {
+                    lo = mid + 1;
+                }
+            }
+            return hi - 1;
         }
 
         protected override string SolvePartTwo()
