f04a057947e69fe5809ab08c30f0da3fa617e8b3
martinsonnberger <martin.sonnberger@icloud.com>
2020-12-10 07:21:33 +0100

    day 10

--- AdventOfCode/Solutions/Year2020/Day10/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day10/Solution.cs
@@ -1,26 +1,62 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace AdventOfCode.Solutions.Year2020
 {
 
     class Day10 : ASolution
     {
+        int jolt1;
+        int jolt3;
+        int current;
+        List<int> list = new List<int>();
 
         public Day10() : base(10, 2020, "")
         {
-
+            list = Input.ToIntArray("\n").ToList();
+            jolt1 = 0;
+            jolt3 = 1;
         }
-
         protected override string SolvePartOne()
         {
-            return null;
+            current = 0;
+            list.Sort();
+
+            foreach (var item in list)
+            {
+                if (item - current == 1)
+                {
+                    jolt1++;
+                }
+                if (item - current == 3)
+                {
+                    jolt3++;
+                }
+                current = item;
+            }
+
+            return (jolt1 * jolt3).ToString();
         }
 
         protected override string SolvePartTwo()
         {
-            return null;
+            int device = list.Max() + 3;
+            list.Add(0);
+            list.Sort();
+            list.Reverse();
+
+            var edges = new Dictionary<int, long> { { device, 1 } };
+            foreach (var item in list)
+            {
+                edges.TryGetValue(item + 1, out long c1);
+                edges.TryGetValue(item + 2, out long c2);
+                edges.TryGetValue(item + 3, out long c3);
+                edges[item] = c1 + c2 + c3;
+            }
+            
+            return edges[0].ToString();
         }
     }
 }
