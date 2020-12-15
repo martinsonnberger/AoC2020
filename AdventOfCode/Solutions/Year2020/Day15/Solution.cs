a2b76e6fcaae235f9589c8c87a5425e13ee534e9
martinsonnberger <martin.sonnberger@icloud.com>
2020-12-15 17:18:35 +0100

    day 15 dictionary solution

--- AdventOfCode/Solutions/Year2020/Day15/Solution.cs
+++ AdventOfCode/Solutions/Year2020/Day15/Solution.cs
@@ -12,42 +12,42 @@ namespace AdventOfCode.Solutions.Year2020
         public Day15() : base(15, 2020, "")
         {
             startNums = Input.ToIntArray(",");
-            //startNums = new int[] {0, 3, 6};
         }
 
         protected override string SolvePartOne()
         {
-            /*
-            int[] memory = new int[30000000];
-            Array.Copy(startNums, memory, startNums.Length);
-
-            for (int i = startNums.Length; i < 30000000; i++)
-            {
-                memory[i] = CheckArray(memory, i);
-                if  (i % 100000 == 0)
-                    Console.WriteLine(i);
-            }
-            return memory[30000000 - 1].ToString();
-            */
-            return null;
+            return lastNumber(2020, startNums).ToString();
         }
-
-        public int CheckArray(int[] array, int index)
-        {
-            int number = array[index - 1];
-            for (int i = index - 2; i >= 0; i--)
-            {
-                if (array[i] == number)
-                {
-                    return index - i - 1;
-                }
-            }
-            return 0;
-        }
-
         protected override string SolvePartTwo()
         {
-            return null;
+            return lastNumber(30000000, startNums).ToString();
+        }
+
+        int lastNumber(int lastIndex, int[] numbers)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                dict.Add(numbers[i], i + 1);
+            }
+
+            int last = numbers[numbers.Length - 1];
+
+            for (int i = numbers.Length + 1; i <= lastIndex; i++)
+            {
+                if (!dict.ContainsKey(last))
+                {
+                    dict[last] = i - 1;
+                    last = 0;
+                }
+                else
+                {
+                    int newNum = i - dict[last] - 1;
+                    dict[last] = i - 1;
+                    last = newNum;
+                }
+            }
+            return last;
         }
     }
 }
